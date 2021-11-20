efe0aff0a6501991fcb0c50e50d0a694f7c4da3c
shaheer8086 <92780355+shaheer8086@users.noreply.github.com>
2021-11-20 11:32:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+
+# Complete project details at https://RandomNerdTutorials.com
+
+def web_page():
+  if led.value() == 1:
+    gpio_state="ON"
+  else:
+    gpio_state="OFF"
+  
+  html = """<html><head> <title>Milestone Door Server</title> <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,"> <style>html{font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
+  h1{color: #0F3376; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #276ceb; border: none; 
+  border-radius: 5px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
+  .button2{background-color: #4286f4;}</style></head><body> <h1>Milestone Door Server</h1><a href="/?led=on"><button class="button">OPEN</button></a></p></a></p></body></html>"""
+  return html
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 80))
+s.listen(5)
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  request = conn.recv(1024)
+  request = str(request)  
+  led_on = request.find('/?led=on')
+
+  led_off = request.find('/?led=on')
+  if led_on == 6:
+    print('LED ON')
+    led.value(1)
+  if led_off == 6:
+    print('LED OFF')
+    led.value(0)
+  response = web_page()
+  conn.send('HTTP/1.1 200 OK\n')
+  conn.send('Content-Type: text/html\n')
+  conn.send('Connection: close\n\n')
+  conn.sendall(response)
+  conn.close()
+
+
+
+
+
+
