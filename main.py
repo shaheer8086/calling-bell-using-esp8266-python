55307b72896f67adb537358330101f31d6046f98
shaheer8086 <92780355+shaheer8086@users.noreply.github.com>
2022-01-25 12:35:08 +0530

    set timout for connection

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def web_page():
   else:
     gpio_state="OFF"
     
+    
+ #uncomment below function for bottom section
+#def web_page():
+ # led.value(1)
+  #time.sleep(0.5)
+  #led.value(0)
+    
     #add the below scipt in html code after <head> open tag,for to close automatically the web page in a time delay
     #<script>setTimeout(function()window.close() }, 100);</script>
   
@@ -46,6 +53,26 @@ while True:
   conn.sendall(response)
   conn.close()
 
+ #uncomment this section for just you want to call that function on top and then automatically close the connection after 3 sec the after waiting for connection from next device
+  #while True:
+  #try:
+   # if gc.mem_free() < 102000:
+      #gc.collect()
+    #conn, addr = s.accept()
+    #conn.settimeout(3.0)
+    #print('Got a connection from %s' % str(addr))
+    #request = conn.recv(1024)
+    #conn.settimeout(None)
+    #print('Content = %s' % str(request))
+    #response = web_page()
+    #conn.send('HTTP/1.1 200 OK\n')
+    #conn.send('Content-Type: text/html\n')
+    #conn.send('Connection: close\n\n')
+    #conn.sendall(response)
+    #conn.close()
+  #except OSError as e:
+    #conn.close()
+    #print('Connection closed')
 
 
 
